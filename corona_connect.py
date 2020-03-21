787141be8108a42a49b32e3ff59eb50d0e2770d7
Jonas Bulik <jonas@bulik.dev>
2020-03-21 15:02:38 +0000

    fix wrong merge

--- corona_connect.py
+++ corona_connect.py
@@ -20,15 +20,11 @@ def phone(event, context):
     helper = Helper[1]
     body = {
         "phone": helper.phone,
-        "name": helper.first_name + " " + helper.last_name,
+        "name": f"{helper.first_name} {helper.last_name}",
         "location": "Berlin Mitte",
     }
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "phone": "+4917696585570",
-            "name": "Jonas",
-            "location": "Berlin Mitte",
-        }),
+        "body": json.dumps(body}),
     }
