f8453c2eb472a47a7dd03e037138aaebdf21eed7
Nils Krüger <kruegern99@gmail.com>
2020-03-21 18:02:10 +0100

    Return closest helper

--- corona_connect.py
+++ corona_connect.py
@@ -1,6 +1,6 @@
 import json
 
-from pony.orm import db_session
+from pony.orm import db_session, select
 
 from db import Helper
 
@@ -17,11 +17,15 @@ def register(event, context):
 
 @db_session
 def phone(event, context):
-    helper = Helper[1]
+    requester_lat = 0.0
+    requester_lon = 0.0
+    best_helpers = select(h for h in Helper) \
+        .order_by(lambda h: (h.lon - requester_lon) ** 2 + (h.lat - requester_lat) ** 2)
+    helper = list(best_helpers)[0]
     body = {
         "phone": helper.phone,
         "name": f"{helper.first_name} {helper.last_name}",
-        "location": "Berlin Mitte",
+        "location": helper.location_name,
     }
 
     return {
